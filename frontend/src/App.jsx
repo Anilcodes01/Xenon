9fc88fac59c9f918a31bd48c92c955672aca289a
Anil Pal <anilcodes01@gmail.com>
2024-10-21 16:12:25 +0530

    fixed bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import "./App.css";
-import Login from "./pages/login/Hogin";
+import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Home from "./pages/home/Home";
 import { Route, Routes } from "react-router-dom";
